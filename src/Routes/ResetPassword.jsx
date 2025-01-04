f649b97430cba94f551c3df08020b30bed875218
clifforddonk <clifforddonk@gmail.com>
2025-01-04 21:00:04 +0000

    first commit

--- src/Routes/ResetPassword.jsx
+++ src/Routes/ResetPassword.jsx
@@ -1,8 +1,32 @@
 const ResetPassword = () => {
   return (
     <div className="">
+      <div className="flex justify-between">
+        <div>
+          <img className="w-[14] h-[14]" src="./image.png" alt="" />
+        </div>
+        <div>
+          <p className="font-semibold text-xl px-4 py-4">English</p>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <img className="w-48 h-48" src="./image2.png" alt="" />
+      </div>
       <div>
-        <img className="w-[60px] h-[53px]" src="./image.png" alt="" />
+        <p className="text-center text-2xl font-bold mt-3">
+          African Youth Festival
+        </p>
+        <p className="text-center mt-3 px-2">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+          temporibus
+        </p>
+      </div>
+      <div>
+        <form action="">
+          <label htmlFor=""></label>
+          <input type="password" name="" id="" />
+        </form>
       </div>
     </div>
   );
